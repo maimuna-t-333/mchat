cd51a652f53e4d2e996b54511f65e152f8b2ce9b
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-05-15 19:42:20 +0600

    Modified register page design

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -48,8 +48,8 @@ export default function RegisterPage() {
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900">
-      <div className="w-full max-w-md p-8 bg-gray-800 rounded-xl shadow-lg">
-        <h2 className="text-2xl font-bold text-white mb-6">
+      <div className="w-full max-w-md mx-4 p-6 lg:p-8 bg-gray-800 rounded-xl shadow-lg">
+        <h2 className="text-2xl text-center font-bold text-white mb-6">
           Create Account
         </h2>
 
@@ -102,18 +102,23 @@ export default function RegisterPage() {
             />
           </div>
 
-          <Button
+          <div className='flex justify-center'> 
+             <Button
             type="submit"
-            className="w-full"
+            className=" px-6 py-2 rounded-full text-white font-semibold shadow-lg hover:cursor-pointer hover:border-2 border-white"
+              style={{ backgroundColor: '#168aad' }}
             disabled={loading}
           >
             {loading ? 'Creating account...' : 'Register'}
           </Button>
+          </div>
+
+         
         </form>
 
         <p className="text-gray-400 text-sm text-center mt-4">
           Already have an account?{' '}
-          <Link href="/login" className="text-blue-400 hover:underline">
+          <Link href="/login" className="text-[#168aad] hover:text-white">
             Login
           </Link>
         </p>
